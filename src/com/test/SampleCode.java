136fce7f8eb408b2aa9b9175b4ab865d794b26f1
aashish.kumar.mor <aashish.kumar.mor@BDC6-L-026KL7Q.dir.svc.accenture.com>
2021-01-06 14:59:34 +0530

    additing new line

--- src/com/test/SampleCode.java
+++ src/com/test/SampleCode.java
@@ -4,8 +4,9 @@ public class SampleCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HEllo World");
+		System.out.println("Hello World");
 		System.out.print(false);
+		System.out.println("test");
 
 	}
 
