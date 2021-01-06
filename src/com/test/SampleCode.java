2ccc6f01822c667d2c18a9c9256b6297158ebd69
aashish.kumar.mor <aashish.kumar.mor@BDC6-L-026KL7Q.dir.svc.accenture.com>
2021-01-06 13:21:06 +0530

    resolved conflict

--- src/com/test/SampleCode.java
+++ src/com/test/SampleCode.java
@@ -5,8 +5,6 @@ public class SampleCode {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HEllo World");
-		System.out.println("Aashish");
-		
 
 	}
 
