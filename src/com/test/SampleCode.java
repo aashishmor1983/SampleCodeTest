7b45fe3602ab11374443365cd8638740e2ebbf2e
aashish.kumar.mor <aashish.kumar.mor@BDC6-L-026KL7Q.dir.svc.accenture.com>
2021-01-06 13:14:44 +0530

    test

--- src/com/test/SampleCode.java
+++ src/com/test/SampleCode.java
@@ -4,9 +4,9 @@ public class SampleCode {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Aashish");
 		System.out.println("HEllo World");
+		System.out.println("Aashish");
+		
 
 	}
 
